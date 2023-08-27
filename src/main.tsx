0e1713b2d4d8d0c9437e62c6a72f8078e75f73c5
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-08-27 12:33:51 -0300

    fix: remove react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import './styles/global.css'
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
     <React.StrictMode>
         <App />
     </React.StrictMode>
-  </QueryClientProvider>
 )
